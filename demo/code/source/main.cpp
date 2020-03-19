7adad9dcabbf32fec75584fc49d84da8a7b18d0e
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2020-03-19 18:58:02 +0100

    template

---
+++ demo/code/source/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Example.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << test();
+    return 0;
+}
---
+++ dllTestLibrary/code/source/Example.cpp
@@ -0,0 +1,7 @@
+#include "..\headers\Example.hpp"
+
+
+EXAMPLE_API const char* test()
+{
+    return "Hello world. Este codigo se esta ejecutando desde una dll.";
+}
